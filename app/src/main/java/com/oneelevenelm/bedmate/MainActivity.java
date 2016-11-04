68c9c0575fcd33eff54f24facb77a1685256b8e5
Jay Whipple <jay@eguestcontrols.com>
2016-11-04 16:54:26 -0600

    Added information to the gitignore and the manifest and main activity

--- app/src/main/java/com/oneelevenelm/bedmate/MainActivity.java
+++ app/src/main/java/com/oneelevenelm/bedmate/MainActivity.java
@@ -2,9 +2,18 @@ package com.oneelevenelm.bedmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.inject.Inject;
 
 import roboguice.activity.RoboActivity;
+import roboguice.context.event.OnCreateEvent;
+import roboguice.context.event.OnDestroyEvent;
+import roboguice.event.Observes;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
+@ContentView(R.layout.activity_main)
 public class MainActivity extends RoboActivity {
 
     @Override
